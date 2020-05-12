563a49e4a274209d5abed4a5afaa7c50a41f0d19
amolnak <33677644+amolnak@users.noreply.github.com>
2020-05-12 13:44:59 +0530

    Initial commit

---
+++ RealWorldApp/RealWorldApp/AppSettings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FoodApp
+{
+    public static class AppSettings
+    {
+        //public static string ApiUrl = "http://192.168.0.105:45455/";
+        public static string ApiUrl = "https://foodapi-ni1.conveyor.cloud/";
+    }
+}
